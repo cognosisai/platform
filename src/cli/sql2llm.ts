c49babd07f5b260e88e2e37c8a36eddc0eb53d20
Matt Busigin <mbusigin@citrisoft.com>
2022-12-16 22:30:33 -0500

    Changing sql2llm over to yargs to pretend that I'm not a barbarian. Added csv and json return type options. GPT-3 wrote most of this, so don't blame me if it doesn't work

--- src/cli/sql2llm.ts
+++ src/cli/sql2llm.ts
@@ -6,6 +6,44 @@ import { argv } from 'process';
 import * as config from '../config';
 import cli_table from 'cli-table3';
 
+const yargs = require('yargs');
+
+const options = yargs
+    .usage('Usage: sql2llm -d <database name> [-n] -q <query> [ -f <file>] [-j] [-c]')
+    .option('d', {
+        alias: 'database-name',
+        description: 'Name of the database to query',
+        type: 'string',
+        demandOption: true
+    })
+    .option('n', {
+        alias: 'natural-language-request',
+        description: 'Option to process <query> as natural language into a nSQL Natural Language Query',
+        type: 'boolean'
+    })
+    .option('q', {
+        alias: 'query',
+        description: 'SQL query to run',
+        type: 'string',
+        demandOption: true
+    })
+    .option('f', {
+        alias: 'file',
+        description: 'Option to include <file> as data to query in context',
+        type: 'string'
+    })
+    .option('j', {
+        alias: 'json',
+        description: 'Return results as JSON instead of displaying with cli-table3',
+        type: 'boolean'
+    })
+    .option('c', {
+        alias: 'csv',
+        description: 'Return results as CSV instead of displaying with cli-table3',
+        type: 'boolean'
+    })
+    .argv;
+
 async function run() {
   const connection = await Connection.connect( {address: config.TEMPORAL_HOST} );
   const client = new WorkflowClient({
@@ -15,16 +53,15 @@ async function run() {
   if ( true ) {
     // load file from disk in argv0
     let text = null;
-    let natural_language_request = true;
-    if ( argv.length == 5 )
+    if ( options.file )
     {
-      const file = fs.readFileSync( argv[4] );
+      const file = fs.readFileSync( options.file );
       text = file.toString();
     }
 
     let handle = await client.start(SQL2LLM, {
       // type inference works! args: [name: string]
-      args: [ argv[2], argv[3], text, natural_language_request ],
+      args: [ options.databaseName, options.query, text, options['natural-language-request'] ],
       taskQueue: 'hello-world',
       // in practice, use a meaningful business id, eg customerId or transactionId
       workflowId: nanoid(),
@@ -37,14 +74,30 @@ async function run() {
       console.log( "No results." );
       process.exit( 0 );
     }
-    let keys = Object.keys( result[0] );
-    var table = new cli_table( {head: keys } );
 
-    result.forEach( (v, i, a) => {
-      table.push( <any> Object.values(v) );
-    });
+    if (options.json) {
+      console.log(JSON.stringify(result, null, 2));
+    } else if (options.csv) {
+      const json2csv = require('json2csv');
 
-    console.log( table.toString() );
+      const fields = Object.keys( result[0] );
+      const opts = { fields };
+      try {
+        const csv = json2csv.parse(result, opts);
+        console.log(csv);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      let keys = Object.keys( result[0] );
+      var table = new cli_table( {head: keys } );
+
+      result.forEach( (v, i, a) => {
+        table.push( <any> Object.values(v) );
+      });
+
+      console.log( table.toString() );
+    }
   }
 
 
